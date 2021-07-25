c2bb185f4b99e394fabee7f28a7108fdbaf9e554
Simon Mok <simonmok@outlook.com>
2021-07-25 13:37:22 +0800

    Code clean up

--- to_sql_diff.py
+++ to_sql_diff.py
@@ -4,15 +4,15 @@ import sys
 
 delimiter = ','
 quotechar = '"'
-file_name_1 = sys.argv[1]
-file_name_2 = sys.argv[2]
+
+file_name_1, file_name_2 = sys.argv[1], sys.argv[2]
 primary_keys = [int(i) for i in sys.argv[3:]]
 table_name = os.path.splitext(file_name_1)[0]
-
 row_number = 0
 record_1 = dict()
 header = None
 primary_key_names = list()
+
 with open(file_name_1, newline='\n') as csv_file:
 	reader = csv.reader(csv_file, delimiter=delimiter, quotechar=quotechar)
 	for row in reader:
@@ -35,21 +35,16 @@ with open(file_name_2, newline='\n') as csv_file:
 			record_2[tuple(row[i] for i in primary_keys)] = row
 		row_number += 1
 
-key_1 = set(record_1)
-key_2 = set(record_2)
-
-diff_added = { k : record_2[k] for k in key_2 - key_1 }
+key_1, key_2 = set(record_1), set(record_2)
 
 print('-- INSERT script')
-
 for key in key_2 - key_1:
-	values = ["'" + column.replace('\n', ' ').replace('\r', '') + "'" for column in record_2[key]]
+	values = ["'" + column + "'" for column in record_2[key]]
 	sql = 'INSERT INTO %s(%s) VALUES (%s);' % (table_name, ', '.join(header), ', '.join(values))
 	print(sql)
 
 print()
 print('-- DELETE script')
-
 for key in key_1 - key_2:
 	condition = ' AND '.join(['%s = %s' % (column, value) for column, value in zip(primary_key_names, key)])
 	sql = 'DELETE FROM %s WHERE %s;' % (table_name, condition)
@@ -57,7 +52,6 @@ for key in key_1 - key_2:
 
 print()
 print('-- UPDATE script')
-
 for key in key_1.intersection(key_2):
 	values_1 = record_1[key]
 	values_2 = record_2[key]
